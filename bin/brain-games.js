15b632373bcb3011edf74d2fa2ac6e534f307b2b
Vladimir L <lototskiy.vladimir@mail.ru>
2023-05-05 16:50:01 +0300

    add .eslint.yml and fix all text file lint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import cli from "../src/cli.js";
+import cli from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 cli();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export default (user) => {
-  const userName = readlineSync.question("May I have your name? ");
-  console.log("Hello, " + userName + "!");
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
